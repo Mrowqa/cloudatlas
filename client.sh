1b6082f3d0ba28883eba906fb8b946291e194f0e
pawel <pawelzell@gmail.com>
2018-11-15 18:19:31 +0100

    files to run rmi

---
+++ client.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Distributed Systems Lab
+# Copyright (C) Konrad Iwanicki, 2012-2014
+#
+# This file contains code samples for the distributed systems
+# course. It is intended for internal use only.
+#
+
+#if [ $# -ne 1 ]; then
+#  echo "Usage: client.sh <N>" >&2
+#  exit 1
+#fi
+
+java -cp $PWD:../../lib/cup.jar:../../lib/JLex.jar -Djava.rmi.server.codebase=file:$PWD -Djava.security.policy=client.policy sr_labs.Sr_labs
+
